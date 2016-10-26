3158b1df70e9701bdec4d741e18444f0bb278d72
Johan Cwiklinski <jcwiklinski@teclib.com>
2016-10-26 14:42:59 +0200

    Add template php files

---
+++ setup.php
@@ -0,0 +1,92 @@
+<?php
+/*
+ -------------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2011 by the INDEPNET Development Team.
+
+ http://teclib.com/   http://glpi-project.org
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of {NAME}.
+
+ {NAME} is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ {NAME} is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with {NAME}. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+ */
+
+define('PLUGIN_{UNAME}_VERSION', '{VERSION}');
+
+/**
+ * Init hooks of the plugin.
+ * REQUIRED
+ *
+ * @return void
+ */
+function plugin_init_{LNAME}() {
+   global $PLUGIN_HOOKS;
+
+   $PLUGIN_HOOKS['csrf_compliant']['{LNAME}'] = true;
+}
+
+
+/**
+ * Get the name and the version of the plugin
+ * REQUIRED
+ *
+ * @return array
+ */
+function plugin_version_{LNAME}() {
+   return [
+      'name'           => '{NAME}',
+      'version'        => PLUGIN_{UNAME}_VERSION,
+      'author'         => '<a href="http://www.teclib.com">Teclib\'</a>',
+      'license'        => '',
+      'homepage'       => '',
+      'minGlpiVersion' => '9.1'
+   ];
+}
+
+/**
+ * Check pre-requisites before install
+ * OPTIONNAL, but recommanded
+ *
+ * @return boolean
+ */
+function plugin_{LNAME}_check_prerequisites() {
+   // Strict version check (could be less strict, or could allow various version)
+   if (version_compare(GLPI_VERSION,'9.1','lt')) {
+      echo "This plugin requires GLPI >= 9.1";
+      return false;
+   }
+   return true;
+}
+
+/**
+ * Check configuration process
+ *
+ * @param boolean $verbose Whether to display message on failure. Defaults to false
+ *
+ * @return boolean
+ */
+function plugin_{LNAME}_check_config($verbose=false) {
+   if (true) { // Your configuration check
+      return true;
+   }
+
+   if ($verbose) {
+      _e('Installed / not configured', '{LNAME}');
+   }
+   return false;
+}
