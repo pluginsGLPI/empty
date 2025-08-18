6584ae046068b316682562b18bb281fec3c0e510
AdrienClairembault <aclairembault@teclib.com>
2025-08-18 12:30:29 +0200

    Add rector

---
+++ rector.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * -------------------------------------------------------------------------
+ * {NAME} plugin for GLPI
+ * -------------------------------------------------------------------------
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * -------------------------------------------------------------------------
+ * @copyright Copyright (C) {YEAR} by the {NAME} plugin team.
+ * @license   MIT https://opensource.org/licenses/mit-license.php
+ * @link      https://github.com/pluginsGLPI/{LNAME}
+ * -------------------------------------------------------------------------
+ */
+
+use Rector\Caching\ValueObject\Storage\FileCacheStorage;
+use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
+
+$configuration = RectorConfig::configure();
+$configuration->withPaths([
+    __DIR__ . '/src',
+    __DIR__ . '/tests',
+    __DIR__ . '/hook.php',
+    __DIR__ . '/setup.php',
+]);
+$configuration->withSkip([
+    __DIR__ . '/tests/bootstrap.php',
+
+    // Can't be used yet, will lead to conflicts with GLPI's core.
+    DeclareStrictTypesRector::class,
+]);
+$configuration->withCache(
+    cacheClass: FileCacheStorage::class,
+    cacheDirectory: __DIR__ . '/var/rector',
+);
+$configuration->withPreparedSets(
+    deadCode: true,
+    codeQuality: true,
+    privatization: true,
+    naming: true,
+    instanceOf: true,
+    earlyReturn: true,
+    strictBooleans: true,
+    carbon: true,
+    rectorPreset: true,
+    phpunitCodeQuality: true,
+    symfonyCodeQuality: true,
+
+    // Our coding styles are already handled by PHP-CS-Fixer
+    codingStyle: false,
+
+    // Type declaration are already handled by PHPStan
+    typeDeclarations: false,
+
+    // We don't use doctrine
+    doctrineCodeQuality: false,
+
+    // Symfony config is handle in GLPI's core
+    symfonyConfigs: false,
+);
+$configuration->withPhpSets();
+return $configuration;
