f2dc6e1b204443ac3607221f008e8aa8f3a712fc
Rom1-B <rom1.biot@gmail.com>
2025-10-29 14:10:26 +0100

    Feat(CI): add rector

---
+++ rector.php
@@ -0,0 +1,99 @@
+<?php
+
+/**
+ * -------------------------------------------------------------------------
+ * {NAME} plugin for GLPI
+ * -------------------------------------------------------------------------
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * -------------------------------------------------------------------------
+ * @license   MIT https://opensource.org/licenses/mit-license.php
+ * @link      https://github.com/pluginsGLPI/{LNAME}
+ * -------------------------------------------------------------------------
+ */
+
+require_once __DIR__ . '/../../src/Plugin.php';
+
+use Rector\Caching\ValueObject\Storage\FileCacheStorage;
+use Rector\CodeQuality\Rector as CodeQuality;
+use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector as DeadCode;
+use Rector\ValueObject\PhpVersion;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withPhpVersion(PhpVersion::PHP_82)
+    ->withCache(
+        cacheClass: FileCacheStorage::class,
+        cacheDirectory: sys_get_temp_dir() . '/empty-rector',
+    )
+    ->withRootFiles()
+    ->withParallel(timeoutSeconds: 300)
+    ->withImportNames(removeUnusedImports: true)
+    ->withRules([
+        CodeQuality\Assign\CombinedAssignRector::class,
+        CodeQuality\BooleanAnd\RemoveUselessIsObjectCheckRector::class,
+        CodeQuality\BooleanAnd\SimplifyEmptyArrayCheckRector::class,
+        CodeQuality\BooleanNot\ReplaceMultipleBooleanNotRector::class,
+        CodeQuality\Catch_\ThrowWithPreviousExceptionRector::class,
+        CodeQuality\Empty_\SimplifyEmptyCheckOnEmptyArrayRector::class,
+        CodeQuality\Expression\InlineIfToExplicitIfRector::class,
+        CodeQuality\Expression\TernaryFalseExpressionToIfRector::class,
+        CodeQuality\For_\ForRepeatedCountToOwnVariableRector::class,
+        CodeQuality\Foreach_\ForeachItemsAssignToEmptyArrayToAssignRector::class,
+        CodeQuality\Foreach_\ForeachToInArrayRector::class,
+        CodeQuality\Foreach_\SimplifyForeachToCoalescingRector::class,
+        CodeQuality\Foreach_\UnusedForeachValueToArrayKeysRector::class,
+        CodeQuality\FuncCall\ChangeArrayPushToArrayAssignRector::class,
+        CodeQuality\FuncCall\CompactToVariablesRector::class,
+        CodeQuality\FuncCall\InlineIsAInstanceOfRector::class,
+        CodeQuality\FuncCall\IsAWithStringWithThirdArgumentRector::class,
+        CodeQuality\FuncCall\RemoveSoleValueSprintfRector::class,
+        CodeQuality\FuncCall\SetTypeToCastRector::class,
+        CodeQuality\FuncCall\SimplifyFuncGetArgsCountRector::class,
+        CodeQuality\FuncCall\SimplifyInArrayValuesRector::class,
+        CodeQuality\FuncCall\SimplifyStrposLowerRector::class,
+        CodeQuality\FuncCall\UnwrapSprintfOneArgumentRector::class,
+        CodeQuality\Identical\BooleanNotIdenticalToNotIdenticalRector::class,
+        CodeQuality\Identical\SimplifyArraySearchRector::class,
+        CodeQuality\Identical\SimplifyConditionsRector::class,
+        CodeQuality\Identical\StrlenZeroToIdenticalEmptyStringRector::class,
+        CodeQuality\If_\CombineIfRector::class,
+        CodeQuality\If_\CompleteMissingIfElseBracketRector::class,
+        CodeQuality\If_\ConsecutiveNullCompareReturnsToNullCoalesceQueueRector::class,
+        CodeQuality\If_\ExplicitBoolCompareRector::class,
+        CodeQuality\If_\ShortenElseIfRector::class,
+        CodeQuality\If_\SimplifyIfElseToTernaryRector::class,
+        CodeQuality\If_\SimplifyIfNotNullReturnRector::class,
+        CodeQuality\If_\SimplifyIfNullableReturnRector::class,
+        CodeQuality\If_\SimplifyIfReturnBoolRector::class,
+        CodeQuality\Include_\AbsolutizeRequireAndIncludePathRector::class,
+        CodeQuality\LogicalAnd\AndAssignsToSeparateLinesRector::class,
+        CodeQuality\LogicalAnd\LogicalToBooleanRector::class,
+        CodeQuality\NotEqual\CommonNotEqualRector::class,
+        CodeQuality\Ternary\UnnecessaryTernaryExpressionRector::class,
+        DeadCode\Assign\RemoveUnusedVariableAssignRector::class,
+    ])
+    ->withPhpSets(php74: true) // apply PHP sets up to PHP 7.4
+;
